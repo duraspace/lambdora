09e362b7600a4ebea3881eab2f7cbd7cec714be9
Daniel Bernstein <dabylon@gmail.com>
2017-10-18 12:31:56 -0700

    Adds sample embedded dynamodb test.

---
+++ lambdora-db/src/test/java/org/fcrepo/lambdora/db/DynamoDbTest.java
@@ -0,0 +1,42 @@
+package org.fcrepo.lambdora.db;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.local.embedded.DynamoDBEmbedded;
+import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author dbernstein
+ */
+public class DynamoDbTest {
+
+    private AmazonDynamoDB dynamodb = null;
+
+    @Before
+    public void setup() {
+        // Create an in-memory and in-process instance of DynamoDB Local that skips HTTP
+        dynamodb = DynamoDBEmbedded.create().amazonDynamoDB();
+    }
+
+    @After
+    public void tearDown() {
+        // Shutdown the thread pools in DynamoDB Local / Embedded
+        if (dynamodb != null) {
+            dynamodb.shutdown();
+        }
+    }
+
+    @Test
+    public void test() throws Exception {
+        listTables(dynamodb.listTables(), "DynamoDB Embedded");
+    }
+
+    private void listTables(final ListTablesResult result, final String method) {
+        System.out.println("found " + Integer.toString(result.getTableNames().size()) + " tables with " + method);
+        for (String table : result.getTableNames()) {
+            System.out.println(table);
+        }
+    }
+}

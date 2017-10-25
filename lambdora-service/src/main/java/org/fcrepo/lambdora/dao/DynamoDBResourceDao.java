96a174768c34faf093799befa3d0cd15a1c274b7
Daniel Bernstein <dabylon@gmail.com>
2017-10-25 13:24:57 -0700

    Removes comment additions from lambdora-service/src/main/java/org/fcrepo/lambdora/dao

--- lambdora-service/src/main/java/org/fcrepo/lambdora/dao/DynamoDBResourceDao.java
+++ lambdora-service/src/main/java/org/fcrepo/lambdora/dao/DynamoDBResourceDao.java
@@ -1,8 +1,6 @@
 package org.fcrepo.lambdora.dao;
 
 /**
- * A DynamoDB-based implementation of ResourceDao.
- *
  * @author bbranan
  */
 public class DynamoDBResourceDao implements ResourceDao {
--- lambdora-service/src/main/java/org/fcrepo/lambdora/dao/ResourceDao.java
+++ lambdora-service/src/main/java/org/fcrepo/lambdora/dao/ResourceDao.java
@@ -1,7 +1,6 @@
 package org.fcrepo.lambdora.dao;
 
 /**
- * A Resource data access object interface.
  * @author bbranan
  */
 public interface ResourceDao {

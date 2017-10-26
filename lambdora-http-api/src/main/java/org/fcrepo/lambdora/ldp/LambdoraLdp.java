bf17ce13c58b8426dc0bbd828d78e92726b7e47c
Graham Triggs <grahamtriggs@gmail.com>
2017-10-26 19:20:54 +0100

    Attempt to return a test resource via getResource through the Jersey layer to get negotiated content

--- lambdora-http-api/src/main/java/org/fcrepo/lambdora/ldp/LambdoraLdp.java
+++ lambdora-http-api/src/main/java/org/fcrepo/lambdora/ldp/LambdoraLdp.java
@@ -1,10 +1,19 @@
 package org.fcrepo.lambdora.ldp;
 
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.NodeFactory;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.fcrepo.http.commons.domain.ContentLocation;
+import org.fcrepo.http.commons.responses.RdfNamespacedStream;
+import org.fcrepo.kernel.api.RdfStream;
 import org.fcrepo.kernel.api.exception.InvalidChecksumException;
 import org.fcrepo.kernel.api.exception.MalformedRdfException;
+import org.fcrepo.kernel.api.rdf.DefaultRdfStream;
 import org.glassfish.jersey.media.multipart.ContentDisposition;
+import org.slf4j.Logger;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
@@ -15,11 +24,16 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import static java.util.stream.Stream.concat;
 import static javax.ws.rs.core.HttpHeaders.CONTENT_DISPOSITION;
 import static javax.ws.rs.core.HttpHeaders.CONTENT_TYPE;
 import static javax.ws.rs.core.HttpHeaders.LINK;
@@ -34,6 +48,7 @@ import static org.fcrepo.http.commons.domain.RDFMediaType.TEXT_HTML_WITH_CHARSET
 import static org.fcrepo.http.commons.domain.RDFMediaType.TEXT_PLAIN_WITH_CHARSET;
 import static org.fcrepo.http.commons.domain.RDFMediaType.TURTLE_WITH_CHARSET;
 import static org.fcrepo.http.commons.domain.RDFMediaType.TURTLE_X;
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Main entry points for Jersey request handling
@@ -42,6 +57,8 @@ import static org.fcrepo.http.commons.domain.RDFMediaType.TURTLE_X;
  */
 @Path("/{path: .*}")
 public class LambdoraLdp {
+    private static final Logger LOGGER = getLogger(LambdoraLdp.class);
+
     @PathParam("path")
     protected String externalPath;
 
@@ -50,6 +67,9 @@ public class LambdoraLdp {
      */
     @Context
     protected HttpHeaders headers;
+
+    @Context protected Request request;
+    @Context protected HttpServletResponse servletResponse;
 
     /**
      * Default JAX-RS entry point
@@ -70,8 +90,21 @@ public class LambdoraLdp {
             N3_WITH_CHARSET, N3_ALT2_WITH_CHARSET, RDF_XML, NTRIPLES, TEXT_PLAIN_WITH_CHARSET,
             TURTLE_X, TEXT_HTML_WITH_CHARSET})
     public Response getResource(@HeaderParam("Range") final String rangeValue) throws IOException {
-        return ok("GET: Welcome to Lambdora. The current time is " + new Date() +
-                ". path=" + externalPath).build();
+
+        /**
+         * Copied and cut down from FedoraLdp
+         *
+         * - Removed all the handling of binary
+         * - Commented out the call to add resource headers
+         * (copying that in expands out the hierarchy of Fedora classes quickly)
+         */
+
+        LOGGER.info("GET resource '{}'", externalPath);
+
+        try (final RdfStream rdfStream = new DefaultRdfStream(testNode())) {
+//            addResourceHttpHeaders(resource());
+            return getContent(rangeValue, getChildrenLimit(), rdfStream);
+        }
     }
 
     /**
@@ -110,4 +143,149 @@ public class LambdoraLdp {
         return ok("POST: Welcome to Lambdora. The current time is " + new Date() +
                 ". path=" + externalPath).build();
     }
+
+    /**
+     * This method returns an HTTP response with content body appropriate to the following arguments.
+     *
+     * @param rangeValue starting and ending byte offsets
+     * @param limit is the number of child resources returned in the response, -1 for all
+     * @param rdfStream to which response RDF will be concatenated
+     * @return HTTP response
+     * @throws IOException in case of error extracting content
+     */
+    protected Response getContent(final String rangeValue,
+                                  final int limit,
+                                  final RdfStream rdfStream) throws IOException {
+
+        /**
+         * Copy(-ish) of FedoraLdp.getContent()
+         *
+         * - getNamespaces() is a stub method (normally, this is rerieved from the FedoraSession()
+         * (Needs to be implemented based on however we are going to deal with namespaces)
+         *
+         * Note the concat of the passed rdfStream (which is just a Node of the "topic"), and a stream
+         * generated from the testModel().
+         *
+         * Without the concat, the test environment just rails with an IllegalStateException
+         */
+        final RdfNamespacedStream outputStream;
+
+        outputStream = new RdfNamespacedStream(
+                new DefaultRdfStream(
+                        rdfStream.topic(),
+                        concat( rdfStream, DefaultRdfStream.fromModel(testNode(), testModel()))
+                ), getNamespaces());
+
+        ;
+
+//        if (prefer != null) {
+//            prefer.getReturn().addResponseHeaders(servletResponse);
+//        }
+        servletResponse.addHeader("Vary", "Accept, Range, Accept-Encoding, Accept-Language");
+
+        return ok(outputStream).build();
+    }
+
+    /**  ********************************
+     *
+     * Stub Methods and data being used for initial development
+     */
+
+    /**
+     * Stub method for testing - return a map of the namespace / uris that you want prefixes for in the RDF
+     * @return
+     */
+    protected Map<String, String> getNamespaces() {
+        Map<String, String> namespaces = new HashMap<>();
+        namespaces.put("premis", "http://www.loc.gov/premis/rdf/v1#");
+        namespaces.put("test", "info:fedora/test/");
+        namespaces.put("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
+        namespaces.put("fedora-model", "info:fedora/fedora-system:def/model#");
+        namespaces.put("ns003", "http://ndl.go.jp/dcndl/terms/");
+        namespaces.put("ns002", "http://www.loc.gov/standards/mods/modsrdf-primer.html#");
+        namespaces.put("ns001", "http://schema.org/");
+        namespaces.put("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+        namespaces.put("xmlns", "http://www.w3.org/2000/xmlns/");
+        namespaces.put("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+        namespaces.put("fedora", "http://fedora.info/definitions/v4/repository#");
+        namespaces.put("xml", "http://www.w3.org/XML/1998/namespace");
+        namespaces.put("ebucore", "http://www.ebu.ch/metadata/ontologies/ebucore/ebucore#");
+        namespaces.put("ldp", "http://www.w3.org/ns/ldp#");
+        namespaces.put("xs", "http://www.w3.org/2001/XMLSchema");
+        namespaces.put("fedoraconfig", "http://fedora.info/definitions/v4/config#");
+        namespaces.put("foaf", "http://xmlns.com/foaf/0.1/");
+        namespaces.put("dc", "http://purl.org/dc/elements/1.1/");
+        return namespaces;
+    }
+
+    /**
+     * Stub method for testing
+     * @return
+     */
+    private int getChildrenLimit() {
+        return 20;
+    }
+
+    /**
+     * Stub method for testing - returns a resource URI for what is being requested.
+     * Hard coded to pretend that we are returning the top level resource for now.
+     * @return
+     */
+    private Node testNode() {
+        return NodeFactory.createURI("http://demo.fcrepo.org:8080/fcrepo/rest/");
+    }
+
+    /**
+     * Stub method for testing - return a Jena Model to create a stream from
+     * In production the stream will have been generated from the service layer
+     * @return
+     */
+    private Model testModel() {
+        final Model model = ModelFactory.createDefaultModel();
+        try (final InputStream in = new ByteArrayInputStream(rootTTL.getBytes("UTF-8")) ) {
+            model.read(in, null, "TURTLE");
+        } catch (IOException e) {
+
+        }
+
+        return model;
+    }
+
+    /**
+     * Sample TURTLE string to provide an RDF model pertaining to a Fedora Resource
+     *
+     * - this was created by curling the Fedora demo server for the root resource
+     */
+
+    private final static String rootTTL = "@prefix premis:  <http://www.loc.gov/premis/rdf/v1#> .\n" +
+"@prefix test:  <info:fedora/test/> .\n" +
+"@prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> .\n" +
+"@prefix fedora-model:  <info:fedora/fedora-system:def/model#> .\n" +
+"@prefix ns003:  <http://ndl.go.jp/dcndl/terms/> .\n" +
+"@prefix ns002:  <http://www.loc.gov/standards/mods/modsrdf-primer.html#> .\n" +
+"@prefix ns001:  <http://schema.org/> .\n" +
+"@prefix xsi:  <http://www.w3.org/2001/XMLSchema-instance> .\n" +
+"@prefix xmlns:  <http://www.w3.org/2000/xmlns/> .\n" +
+"@prefix rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n" +
+"@prefix fedora:  <http://fedora.info/definitions/v4/repository#> .\n" +
+"@prefix xml:  <http://www.w3.org/XML/1998/namespace> .\n" +
+"@prefix ebucore:  <http://www.ebu.ch/metadata/ontologies/ebucore/ebucore#> .\n" +
+"@prefix ldp:  <http://www.w3.org/ns/ldp#> .\n" +
+"@prefix xs:  <http://www.w3.org/2001/XMLSchema> .\n" +
+"@prefix fedoraconfig:  <http://fedora.info/definitions/v4/config#> .\n" +
+"@prefix foaf:  <http://xmlns.com/foaf/0.1/> .\n" +
+"@prefix dc:  <http://purl.org/dc/elements/1.1/> .\n" +
+"\n" +
+"<http://demo.fcrepo.org:8080/fcrepo/rest/>\n" +
+"    fedora:lastModified            \"2017-10-25T16:15:21.355Z\"^^<http://www.w3.org/2001/XMLSchema#dateTime> ;\n" +
+"    rdf:type                       ldp:RDFSource ;\n" +
+"    rdf:type                       ldp:Container ;\n" +
+"    rdf:type                       ldp:BasicContainer ;\n" +
+"    fedora:writable                true ;\n" +
+"    rdf:type                       fedora:RepositoryRoot ;\n" +
+"    rdf:type                       fedora:Resource ;\n" +
+"    rdf:type                       fedora:Container ;\n" +
+"    ldp:contains                   <http://demo.fcrepo.org:8080/fcrepo/rest/test> ;\n" +
+"    ldp:contains                   <http://demo.fcrepo.org:8080/fcrepo/rest/test2> ;\n" +
+"    fedora:hasTransactionProvider  <http://demo.fcrepo.org:8080/fcrepo/rest/fcr:tx> .\n";
 }
---
+++ lambdora-http-api/src/test/java/org/fcrepo/lambdora/ldp/LambdoraLdpTest.java
@@ -0,0 +1,142 @@
+package org.fcrepo.lambdora.ldp;
+
+import com.google.common.collect.ImmutableSet;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.RDFNode;
+import org.fcrepo.http.api.responses.StreamingHtmlProvider;
+import org.fcrepo.http.commons.responses.RdfNamespacedStream;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.io.IOException;
+import java.util.List;
+
+import static javax.ws.rs.core.HttpHeaders.LINK;
+import static javax.ws.rs.core.Response.Status.OK;
+import static org.fcrepo.kernel.api.RdfCollectors.toModel;
+import static org.fcrepo.kernel.api.RdfLexicon.LDP_NAMESPACE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
+
+/**
+ * Notes
+ *
+ * This (what exists of it) is mostly copied from FedoraLdpTest, to try and be able to see what is happening
+ * through the Jersey layer, without deploying it.
+ *
+ * It doesn't currently pass checkstyle (was testing with it disabled just to make progress without worrying about
+ * how it would finally be structured).
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class LambdoraLdpTest {
+    private LambdoraLdp testObj;
+
+    @Mock
+    private Request mockRequest;
+
+    private HttpServletResponse mockResponse;
+
+    /**
+     * Testing the get getResource method
+     *
+     * NB I was hoping to test the serialization after a response is handed to Jersey, but turns
+     * out that isn't what this method does
+     *
+     * In a test environemt, getResource will return with a valid Response.
+     * However, in AWS, it is still giving an Internal Server Error (presumably due to the
+     * transformation providers in Jersey)
+     */
+    @Test
+    public void testGet() throws Exception {
+        /**
+         * Not currently dealing with FedoraReource classes
+         */
+//        setResource(FedoraResource.class);
+
+
+        final Response actual = testObj.getResource(null);
+        assertEquals(OK.getStatusCode(), actual.getStatus());
+
+
+        /**
+         * getResource doesn't add any headers at the moment
+         */
+//        assertTrue("Should have a Link header", mockResponse.containsHeader(LINK));
+//        assertTrue("Should have an Allow header", mockResponse.containsHeader("Allow"));
+//        assertTrue("Should be an LDP Resource",
+//                mockResponse.getHeaders(LINK).contains("<" + LDP_NAMESPACE + "Resource>;rel=\"type\""));
+
+        try (final RdfNamespacedStream entity = (RdfNamespacedStream) actual.getEntity()) {
+            final Model model = entity.stream.collect(toModel());
+            final List<String> rdfNodes = model.listObjects().mapWith(RDFNode::toString).toList();
+            assertTrue("Expected RDF contexts missing", rdfNodes.containsAll(ImmutableSet.of(
+                    "LDP_CONTAINMENT", "LDP_MEMBERSHIP", "PROPERTIES", "SERVER_MANAGED")));
+        }
+    }
+
+    @Before
+    public void setUp() {
+        /**
+         * Prepare the test environment
+         *
+         * LambdoraLdp doesn't currently have a lot of the fields that are injected into a full
+         * FedoraLdp class hierarchy
+         */
+        testObj = spy(new LambdoraLdp());
+
+        mockResponse = new MockHttpServletResponse();
+
+//        idTranslator = new HttpResourceConverter(mockSession,
+//                UriBuilder.fromUri("http://localhost/fcrepo/{path: .*}"));
+
+//        setField(testObj, "request", mockRequest);
+        setField(testObj, "servletResponse", mockResponse);
+//        setField(testObj, "uriInfo", getUriInfoImpl());
+//        setField(testObj, "headers", mockHeaders);
+//        setField(testObj, "idTranslator", idTranslator);
+//        setField(testObj, "nodeService", mockNodeService);
+//        setField(testObj, "containerService", mockContainerService);
+//        setField(testObj, "binaryService", mockBinaryService);
+//        setField(testObj, "httpConfiguration", mockHttpConfiguration);
+//        setField(testObj, "session", mockSession);
+//        setField(testObj, "securityContext", mockSecurityContext);
+//        setField(testObj, "lockManager", mockLockManager);
+
+//        when(mockHttpConfiguration.putRequiresIfMatch()).thenReturn(false);
+
+//        when(mockContainer.getEtagValue()).thenReturn("");
+//        when(mockContainer.getPath()).thenReturn(path);
+//        when(mockContainer.getDescription()).thenReturn(mockContainer);
+//        when(mockContainer.getDescribedResource()).thenReturn(mockContainer);
+
+//        when(mockNonRdfSourceDescription.getEtagValue()).thenReturn("");
+//        when(mockNonRdfSourceDescription.getPath()).thenReturn(binaryDescriptionPath);
+//        when(mockNonRdfSourceDescription.getDescribedResource()).thenReturn(mockBinary);
+
+//        when(mockBinary.getEtagValue()).thenReturn("");
+//        when(mockBinary.getPath()).thenReturn(binaryPath);
+//        when(mockBinary.getDescription()).thenReturn(mockNonRdfSourceDescription);
+
+//        when(mockHeaders.getHeaderString("user-agent")).thenReturn("Test UserAgent");
+
+//        when(mockLockManager.lockForRead(any())).thenReturn(mockLock);
+//        when(mockLockManager.lockForWrite(any(), any(), any())).thenReturn(mockLock);
+//        when(mockLockManager.lockForDelete(any())).thenReturn(mockLock);
+//        when(mockSession.getId()).thenReturn("foo1234");
+//        when(mockSession.getFedoraSession()).thenReturn(mockFedoraSession);
+    }
+}

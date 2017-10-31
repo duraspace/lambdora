e9477522dc7f1220353d7a056c6f7c28219335f8
Bill Branan <bbranan@duraspace.org>
2017-10-31 18:16:29 -0400

    Adds logging to provide details about the HTTP request

--- lambdora-http-api/src/main/java/org/fcrepo/lambdora/ldp/JerseyRequestHandler.java
+++ lambdora-http-api/src/main/java/org/fcrepo/lambdora/ldp/JerseyRequestHandler.java
@@ -5,6 +5,9 @@ import com.amazonaws.serverless.proxy.internal.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.jersey.JerseyLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * AWS Request Handler to proxy requests through to Jersey
@@ -13,6 +16,8 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
  */
 public class JerseyRequestHandler implements RequestHandler<AwsProxyRequest,AwsProxyResponse> {
     private JerseyLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    private static final Logger LOGGER = getLogger(JerseyRequestHandler.class);
 
     /**
      * Initialise the request handler with the AWS Proxy
@@ -30,6 +35,28 @@ public class JerseyRequestHandler implements RequestHandler<AwsProxyRequest,AwsP
      */
     @Override
     public AwsProxyResponse handleRequest(final AwsProxyRequest awsProxyRequest, final Context context) {
+        // Captures the HTTP request in the log
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("\n\n*********** BEGIN HTTP REQUEST ***********" +
+                         "\nHTTP Method={}" +
+                         "\nPath={}" +
+                         "\nResource={}" +
+                         "\nPath Parameters={}" +
+                         "\nQuery String Parameters={}" +
+                         "\nStage Variables={}" +
+                         "\nHeaders={}" +
+                         "\nBody={}" +
+                         "\n*********** END HTTP REQUEST ***********\n",
+                         awsProxyRequest.getHttpMethod(),
+                         awsProxyRequest.getPath(),
+                         awsProxyRequest.getResource(),
+                         awsProxyRequest.getPathParameters(),
+                         awsProxyRequest.getQueryStringParameters(),
+                         awsProxyRequest.getStageVariables(),
+                         awsProxyRequest.getHeaders(),
+                         awsProxyRequest.getBody());
+        }
+
         handler.stripBasePath("/rest");
         return handler.proxy(awsProxyRequest, context);
     }

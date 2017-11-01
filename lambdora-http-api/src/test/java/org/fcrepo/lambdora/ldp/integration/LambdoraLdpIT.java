52efd5cfeefa907d6d931edd111e1a4eed20b83e
Daniel Bernstein <dabylon@gmail.com>
2017-11-01 10:09:58 -0700

    Adds  HEAD and GET methods with minimal implmentations and integration tests.

--- lambdora-http-api/src/test/java/org/fcrepo/lambdora/ldp/integration/LambdoraLdpIT.java
+++ lambdora-http-api/src/test/java/org/fcrepo/lambdora/ldp/integration/LambdoraLdpIT.java
@@ -19,7 +19,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static javax.ws.rs.core.Response.Status.CREATED;
+import static javax.ws.rs.core.Response.Status.METHOD_NOT_ALLOWED;
 import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+import static javax.ws.rs.core.Response.Status.NO_CONTENT;
 import static javax.ws.rs.core.Response.Status.OK;
 import static org.junit.Assert.assertEquals;
 
@@ -73,12 +75,7 @@ public class LambdoraLdpIT extends IntegrationTestBase {
 
     @Test
     public void testCreateResource() {
-        final AwsProxyRequest request = buildRequest("/rest/", "POST");
-        final Map<String, String> headers = new HashMap<>();
-        headers.put("Slug", "test");
-        request.setHeaders(headers);
-        final AwsProxyResponse response = handler.handleRequest(request, lambdaContext);
-        assertEquals("resource should have been created", CREATED.getStatusCode(), response.getStatusCode());
+        createResource("test");
         final AwsProxyResponse getResponse = handler.handleRequest(buildGetRequest("/rest/test"), lambdaContext);
         assertEquals("newly created resource should exist", OK.getStatusCode(), getResponse.getStatusCode());
     }
@@ -95,6 +92,60 @@ public class LambdoraLdpIT extends IntegrationTestBase {
             lambdaContext);
 
         assertEquals("newly created resource should exist", OK.getStatusCode(), getResponse.getStatusCode());
+    }
+
+    @Test
+    public void testHeadOnRoot() {
+        final AwsProxyRequest request = buildRequest("/rest/", "HEAD");
+        final AwsProxyResponse response = handler.handleRequest(request, lambdaContext);
+        assertEquals("root should allow head", OK.getStatusCode(), response.getStatusCode());
+    }
+
+    @Test
+    public void testHeadOnExistentResource() {
+        createResource("test");
+        final AwsProxyRequest request2 = buildRequest("/rest/test", "HEAD");
+        final AwsProxyResponse response2 = handler.handleRequest(request2, lambdaContext);
+        assertEquals("resource should return OK on HEAD", OK.getStatusCode(), response2.getStatusCode());
+    }
+
+    @Test
+    public void testHeadOnNonExistentResource() {
+        final AwsProxyRequest request = buildRequest("/rest/does-not-exist", "HEAD");
+        final AwsProxyResponse response = handler.handleRequest(request, lambdaContext);
+        assertEquals("resource should not exist", NOT_FOUND.getStatusCode(), response.getStatusCode());
+    }
+
+    @Test
+    public void testPutOnRoot() {
+        final AwsProxyRequest request = buildRequest("/rest/", "PUT");
+        final AwsProxyResponse response = handler.handleRequest(request, lambdaContext);
+        assertEquals("root should allow put", METHOD_NOT_ALLOWED.getStatusCode(), response.getStatusCode());
+    }
+
+    @Test
+    public void testPutOnNonExistentResource() {
+        final AwsProxyRequest request = buildRequest("/rest/test", "PUT");
+        final AwsProxyResponse response = handler.handleRequest(request, lambdaContext);
+        assertEquals("new resource should allow put", CREATED.getStatusCode(), response.getStatusCode());
+    }
+
+    @Test
+    public void testPutOnExistentResource() {
+        final String resource = "resource";
+        createResource(resource);
+        final AwsProxyRequest request = buildRequest("/rest/" + resource, "PUT");
+        final AwsProxyResponse response = handler.handleRequest(request, lambdaContext);
+        assertEquals("resource should return NO_CONTENT on PUT", NO_CONTENT.getStatusCode(), response.getStatusCode());
+    }
+
+    private void createResource(final String resourcePath) {
+        final AwsProxyRequest request1 = buildRequest("/rest/", "POST");
+        final Map<String, String> headers = new HashMap<>();
+        headers.put("Slug", resourcePath);
+        request1.setHeaders(headers);
+        final AwsProxyResponse response1 = handler.handleRequest(request1, lambdaContext);
+        assertEquals("resource should have been created", CREATED.getStatusCode(), response1.getStatusCode());
     }
 
     private AwsProxyRequest buildRequest(final String path, final String method) {

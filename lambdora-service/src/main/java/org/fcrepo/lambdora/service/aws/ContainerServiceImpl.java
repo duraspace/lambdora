5d66c6dc708c3c1ffe3605953f8186fc09c3718c
Daniel Bernstein <dabylon@gmail.com>
2017-11-02 13:28:29 -0700

    Adds support for ldp:contains and fedora:hasParent predicates.

--- lambdora-service/src/main/java/org/fcrepo/lambdora/service/aws/ContainerServiceImpl.java
+++ lambdora-service/src/main/java/org/fcrepo/lambdora/service/aws/ContainerServiceImpl.java
@@ -28,22 +28,46 @@ public class ContainerServiceImpl extends FedoraResourceServiceBase<Container> i
         super(dao);
     }
 
+    private static URI getParent(final URI uri) {
+        if (uri.getPath().equals("") || uri.getPath().equals("/")) {
+            return null;
+        } else {
+            return uri.getPath().endsWith("/") ? uri.resolve("..") : uri.resolve(".");
+        }
+    }
+
     @Override
     protected Container create(final URI identifier) {
         LOGGER.debug("Create: {}", identifier);
+        final String identifierStr = identifier.toString();
+        final URI parent = getParent(identifier);
 
         final ResourceTripleDao dao = getResourceTripleDao();
+
         dao.addResourceTriple(toResourceTriple(identifier,
-            new Triple(createURI(identifier.toString()),
-                       createURI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),
-                       createURI("http://www.w3.org/ns/ldp#Container"))));
+            new Triple(createURI(identifierStr),
+                createURI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),
+                createURI("http://www.w3.org/ns/ldp#Container"))));
         dao.addResourceTriple(toResourceTriple(identifier,
-            new Triple(createURI(identifier.toString()),
+            new Triple(createURI(identifierStr),
                 createURI("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),
                 createURI("http://www.w3.org/ns/ldp#RDFSource"))));
 
+
         //TODO recursively create parents if do not exist, adding ldp:contains with a reference to child
-        final Container container =  new ContainerImpl(identifier, dao);
+        final Container container = new ContainerImpl(identifier, dao);
+        if (parent != null) {
+            final String parentStr = parent.toString();
+            dao.addResourceTriple(toResourceTriple(identifier,
+                new Triple(createURI(identifierStr),
+                    createURI("http://fedora.info/definitions/v4/repository#hasParent"),
+                    createURI(parentStr))));
+
+            dao.addResourceTriple(toResourceTriple(parent,
+                new Triple(createURI(parentStr),
+                    createURI("http://www.w3.org/ns/ldp#contains"),
+                    createURI(identifierStr))));
+        }
         return container;
     }
 }
